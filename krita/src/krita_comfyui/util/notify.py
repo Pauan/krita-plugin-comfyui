c99e9febe3b8aec3d7377acdbfd7023402afc852
Pauan <pauanyu+github@pm.me>
2026-05-30 14:25:34 -0700

    Fixing bug with notify

--- krita/src/krita_comfyui/util/notify.py
+++ krita/src/krita_comfyui/util/notify.py
@@ -20,12 +20,21 @@ class NotifyWorker(QObject):
         self.notifier = None
         self.message.connect(self.on_message)
 
+        self.event_loop = None
+
     @pyqtSlot(str)
     def on_message(self, message: str):
+        if self.event_loop is None:
+            self.event_loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.event_loop)
+
         if self.notifier is None:
             self.notifier = desktop_notifier.DesktopNotifier(
                 app_name="Krita ComfyUI",
                 app_icon=None,
             )
 
-        asyncio.run(notify(self.notifier, message))
+        try:
+            self.event_loop.run_until_complete(notify(self.notifier, message))
+        except Exception as e:
+            print(str(e))
